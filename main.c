d27db3c5aefced0e190454d8e16836a926def6c7
rysealex <aryse54@gmail.com>
2024-12-11 00:30:10 -0600

    Error checked for non-letter input, implemented total time taken, added color features, and total attempts.

--- main.c
+++ main.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
+#include <time.h>
 #include <unistd.h>
 
-#define ASCII_A 97  // ascii value of 'a'
-#define ASCII_Z 122 // ascii value of 'z'
+#define ASCII_A 97   // ascii value of 'a'
+#define ASCII_Z 122  // ascii value of 'z'
+#define COLOR_MIN 31 // min color value
+#define COLOR_MAX 37 // max color value
 
 /*
     this program will attempt to match the user input word repeatedly until successful
@@ -16,8 +20,10 @@ int main(void)
 {
     // declare and initialize variables here
     char *word = NULL, *wordCheck = NULL, *result = NULL;
-    size_t len = 0, lenCheck = 0, buffer = 100;
+    size_t len = 0, lenCheck = 0, buffer = 100, attempts = 0;
     int ch;
+    clock_t start, end;
+    double time_taken;
 
     printf("Enter a word: "); // prompt the user
 
@@ -25,12 +31,17 @@ int main(void)
 
     if (word == NULL)
     {
-        printf("Memory allocation failed\n");
+        printf("\033[31mMemory allocation failed\033[0m\n"); // red text here
         return 1;
     } // error checking here
 
     while ((ch = getchar()) != '\n' && ch != EOF)
     {
+        if (!isalpha(ch))
+        {
+            printf("\033[31mEnter only letters\033[0m\n"); // red text here
+            return 1;
+        } // error checking here
         word[len++] = (char)ch;
     } // adding elements to word and finding length of word
 
@@ -38,7 +49,7 @@ int main(void)
 
     if (len == 0)
     {
-        printf("No word entered\n");
+        printf("\033[31mNo word entered\033[0m\n"); // red text here
         return 1;
     } // error checking here
 
@@ -50,7 +61,7 @@ int main(void)
 
     if (wordCheck == NULL)
     {
-        printf("Memory allocation failed\n");
+        printf("\033[31mMemory allocation failed\033[0m\n"); // red text here
         free(word);
         return 1;
     } // error checking here
@@ -64,7 +75,7 @@ int main(void)
 
     if (len != lenCheck || strcmp(word, wordCheck) != 0)
     {
-        printf("Incorrect word input\n");
+        printf("\033[31mIncorrect word input\033[0m\n"); // red text here
         free(word);
         free(wordCheck);
         return 1;
@@ -74,27 +85,37 @@ int main(void)
 
     if (result == NULL)
     {
-        printf("Memory allocation failed\n");
+        printf("\033[31mMemory allocation failed\033[0m\n"); // red text here
         free(word);
         free(wordCheck);
         return 1;
     } // error checking here
 
+    start = clock(); // start he clock
+
     for (int i = 0; i < len; i++)
     {
         while (1)
         {
-            char letter = rand() % (ASCII_Z - ASCII_A + 1) + ASCII_A; // random char in the alphabet
-            result[i] = letter;
-            printf("%s\n", result);
-            if (letter == word[i])
+            attempts++;
+            char randLetter = rand() % (ASCII_Z - ASCII_A + 1) + ASCII_A; // random char in the alphabet
+            result[i] = randLetter;
+            int randColor = rand() % (COLOR_MAX - COLOR_MIN + 1) + COLOR_MIN; // random color value
+            printf("\033[%dm%s\033[0m\n", randColor, result);
+            if (randLetter == word[i])
             {
-                result[i] = letter;
+                result[i] = randLetter;
                 break;
             } // if matches
             usleep(10000);
         } // continue until current element of word is found
     } // iterate for each element in word
+
+    end = clock(); // stop the clock
+
+    time_taken = ((double)(end - start)) / CLOCKS_PER_SEC; // calculate time here
+
+    printf("\033[32mTook %ld attempts in %f seconds\033[0m\n", attempts, time_taken); // display to the user
 
     result[len] = '\0'; // null terminate the result
 
